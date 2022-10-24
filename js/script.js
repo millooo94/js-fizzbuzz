8711fbcf20f758961387b7256f7b4a9483432a52
millooo94 <camillomusmeciunict@gmail.com>
2022-10-24 16:53:55 +0200

    Refactor color

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
 const eleSquares = document.querySelector('.squares')
 
+
+
+
+
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        eleSquares.innerHTML += `<div class="square">FIZZBUZZ</div>`;
-        // let eleSquare = document.querySelectorAll('square')
-        // eleSquare.style.backgroundColor = "yellow"
+        eleSquares.innerHTML += `<div class="square red">FIZZBUZZ</div>`;
+
 
     } else if  (i % 5 == 0) {
-        eleSquares.innerHTML += `<div class="square">BUZZ</div>`;
+        eleSquares.innerHTML += `<div class="square yellow">BUZZ</div>`;
 
 
     } else if  (i % 3 === 0) {
-        eleSquares.innerHTML += ` <div class="square">FIZZ</div>`;
+        eleSquares.innerHTML += ` <div class="square green">FIZZ</div>`;
 
 
     } else {
-        eleSquares.innerHTML += `<div class="square">${i}</div>`;
+        eleSquares.innerHTML += `<div class="square blue">${i}</div>`;
  
     }
-
 }
+
+let red = document.querySelectorAll('.squares .square.red')
+red.style.backgroundColor = 'red'
+
+
+
